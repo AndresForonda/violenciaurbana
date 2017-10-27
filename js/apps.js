104f478e3f2c86224a7ac5f11bd8b66882c8853e
DoitNT <andresforonda.af@gmail.com>
2017-10-27 18:06:34 -0500

    Se agrega nuevas secciones

---
+++ js/apps.js
@@ -0,0 +1,45 @@
+var nav=document.getElementById('nav');
+function myFunction(x) {
+    nav.classList.toggle('mostrar');
+    nav.classList.toggle('fixed');
+    x.classList.toggle('fixed-button');
+}
+
+/*Modal*/
+	function openModal() {
+	  document.getElementById('myModal').style.display = "block";
+	}
+
+	function closeModal() {
+	  document.getElementById('myModal').style.display = "none";
+	}
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("demo");
+	  var captionText = document.getElementById("caption");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	  captionText.innerHTML = dots[slideIndex-1].alt;
+	};
+/*Fin modal*/
