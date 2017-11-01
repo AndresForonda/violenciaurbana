6260e4d9e40f833611a06dd3943309e10e33cf37
DoitNT <andresforonda.af@gmail.com>
2017-11-01 12:55:13 -0500

    Se agrega funcionalidad a las flechas del teclado para cambiar fotos del modal. La galería fotográfica ya esta lista para la vista móvil.

--- js/apps.js
+++ js/apps.js
@@ -29,7 +29,6 @@ function myFunction(x) {
 	  var i;
 	  var slides = document.getElementsByClassName("mySlides");
 	  var dots = document.getElementsByClassName("demo");
-	  var captionText = document.getElementById("caption");
 	  if (n > slides.length) {slideIndex = 1}
 	  if (n < 1) {slideIndex = slides.length}
 	  for (i = 0; i < slides.length; i++) {
@@ -40,6 +39,19 @@ function myFunction(x) {
 	  }
 	  slides[slideIndex-1].style.display = "block";
 	  dots[slideIndex-1].className += " active";
-	  captionText.innerHTML = dots[slideIndex-1].alt;
 	};
 /*Fin modal*/
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+       plusSlides(-1);
+    }
+    else if (e.keyCode == '39') {
+       plusSlides(1);
+    }
+
+}
